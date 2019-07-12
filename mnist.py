7f54495b8ee816971e32ad9a9fa995c02a7e53a9
Mohammed <m.mochoui@gmail.com>
2019-07-12 22:24:34 +0200

    third commit

--- mnist.py
+++ mnist.py
@@ -229,7 +229,7 @@ def go(options):
 
             for i in range(10):
                 ax = plt.subplot(4, 10, i + 1)
-                ax.imshow(np.moveaxis(test_edge[i, :, :, :].cpu().squeeze(), 1, -1)) #, cmap='gray'
+                ax.imshow(np.moveaxis(test_edge[i, :, :, :].cpu().numpy().squeeze(), 1, -1)) #, cmap='gray'
                 ptutil.clean(ax)
 
                 if options.loss != 'xent':
